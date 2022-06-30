679431ce9583db49e6eb06947cd0c3da395d0087
Adrian Jarc <adrianjarc1@gmail.com>
2022-06-30 12:14:42 +0200

    fix: build

--- dist/commands/ecs-deploy.command.js
+++ dist/commands/ecs-deploy.command.js
@@ -202,7 +202,7 @@ exports.command = {
             }, {})
             : {};
         // override task container secrets from config.yaml
-        if (argv.config.ecsSecrets && typeof argv.config.ecSecrets === "object") {
+        if (argv.config.ecsSecrets && typeof argv.config.ecsSecrets === "object") {
             for (const [secretKey, secretFrom] of Object.entries(argv.config.ecsSecrets)) {
                 taskDefinitionContainerSecrets[secretKey] = secretFrom;
             }
